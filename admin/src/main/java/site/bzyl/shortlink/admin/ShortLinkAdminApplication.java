1eda40c4e6c63e4aa03e6e2bf36d0686a27d4752
Cymbospondylus <2810560593@qq.com>
2023-11-06 09:56:44 +0800

    添加依赖, 搭建基本框架

---
+++ admin/src/main/java/site/bzyl/shortlink/admin/ShortLinkAdminApplication.java
@@ -0,0 +1,17 @@
+package site.bzyl.shortlink.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Classname ShortLinkAdminApplication
+ * @Description 后台管理启动类
+ * @Date 2023/11/6 9:42
+ * @Created by cymbospondylus
+ */
+@SpringBootApplication
+public class ShortLinkAdminApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ShortLinkAdminApplication.class, args);
+    }
+}
---
+++ project/src/main/java/site/bzyl/shortlink/project/ShortLinkProjectApplication.java
@@ -0,0 +1,17 @@
+package site.bzyl.shortlink.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Classname ShortLinkProjectApplication
+ * @Description 后台项目启动类
+ * @Date 2023/11/6 9:42
+ * @Created by cymbospondylus
+ */
+@SpringBootApplication
+public class ShortLinkProjectApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ShortLinkProjectApplication.class, args);
+    }
+}
