88898f8a05069bf872a6e917b373594c7f7fa52d
Cymbospondylus <2810560593@qq.com>
2023-11-08 20:49:31 +0800

    feature: 开发短链接后管用户字段自动填充功能

---
+++ admin/src/main/java/site/bzyl/shortlink/admin/config/MyMetaObjectHandler.java
@@ -0,0 +1,27 @@
+package site.bzyl.shortlink.admin.config;
+
+import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.ibatis.reflection.MetaObject;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+
+/**
+ * 元数据自动填充
+ */
+@Slf4j
+@Component
+public class MyMetaObjectHandler implements MetaObjectHandler {
+
+    @Override
+    public void insertFill(MetaObject metaObject) {
+        this.strictInsertFill(metaObject, "createTime", () -> LocalDateTime.now(), LocalDateTime.class);
+        this.strictInsertFill(metaObject, "updateTime", () -> LocalDateTime.now(), LocalDateTime.class);
+    }
+
+    @Override
+    public void updateFill(MetaObject metaObject) {
+        this.strictUpdateFill(metaObject, "updateTime", () -> LocalDateTime.now(), LocalDateTime.class); // 起始版本 3.3.3(推荐)
+    }
+}
