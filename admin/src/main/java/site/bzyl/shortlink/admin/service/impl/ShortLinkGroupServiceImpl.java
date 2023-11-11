5b6c1249bcf507ea6becaaa1f9a104b06ccdf501
Cymbospondylus <2810560593@qq.com>
2023-11-11 16:46:27 +0800

    fix: 修改短链接后台管理系统新增分组时从用户上下文获取用户名

--- admin/src/main/java/site/bzyl/shortlink/admin/service/impl/ShortLinkGroupServiceImpl.java
+++ admin/src/main/java/site/bzyl/shortlink/admin/service/impl/ShortLinkGroupServiceImpl.java
@@ -3,14 +3,17 @@ package site.bzyl.shortlink.admin.service.impl;
 import cn.hutool.core.bean.BeanUtil;
 import cn.hutool.core.util.RandomUtil;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.conditions.update.LambdaUpdateWrapper;
 import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import org.springframework.stereotype.Service;
 import site.bzyl.shortlink.admin.common.biz.user.UserContext;
+import site.bzyl.shortlink.admin.common.convention.exception.ClientException;
 import site.bzyl.shortlink.admin.common.convention.exception.ServiceException;
 import site.bzyl.shortlink.admin.dao.entity.GroupDO;
 import site.bzyl.shortlink.admin.dao.mapper.ShortLinkGroupMapper;
 import site.bzyl.shortlink.admin.dto.req.ShortLinkGroupSaveReqDTO;
+import site.bzyl.shortlink.admin.dto.req.ShortLinkGroupUpdateReqDTO;
 import site.bzyl.shortlink.admin.dto.resp.ShortLinkGroupRespDTO;
 import site.bzyl.shortlink.admin.service.ShortLinkGroupService;
 
@@ -39,14 +42,14 @@ public class ShortLinkGroupServiceImpl extends ServiceImpl<ShortLinkGroupMapper,
 
         GroupDO shortLinkGroupDO = GroupDO.builder()
                 .gid(gid)
-                .username(requestParam.getUsername())
+                .username(UserContext.getUsername())
                 .name(requestParam.getName())
                 .sortOrder(DEFAULT_SORT_ORDER)
                 .build();
 
         int insert = baseMapper.insert(shortLinkGroupDO);
         if (insert < 1) {
-            ServiceException.cast("短链接新增失败");
+            ServiceException.cast("短链接分组新增失败");
         }
 
     }
@@ -59,5 +62,23 @@ public class ShortLinkGroupServiceImpl extends ServiceImpl<ShortLinkGroupMapper,
         List<GroupDO> groupDOList = baseMapper.selectList(queryWrapper);
 
         return BeanUtil.copyToList(groupDOList, ShortLinkGroupRespDTO.class);
+    }
+
+    @Override
+    public void updateShortLinkGroup(ShortLinkGroupUpdateReqDTO requestParam) {
+        if (!requestParam.getUsername().equals(UserContext.getUsername())) {
+            ClientException.cast("分组不属于当前登录用户");
+        }
+        LambdaUpdateWrapper<GroupDO> updateWrapper = Wrappers.lambdaUpdate(GroupDO.class)
+                .eq(GroupDO::getGid, requestParam.getGid())
+                .eq(GroupDO::getUsername, requestParam.getUsername());
+        GroupDO groupDO = GroupDO.builder()
+                .name(requestParam.getName())
+                .sortOrder(requestParam.getSortOrder())
+                .build();
+        int update = baseMapper.update(groupDO, updateWrapper);
+        if (update < 1) {
+            ServiceException.cast("分组更新失败");
+        }
     }
 }
