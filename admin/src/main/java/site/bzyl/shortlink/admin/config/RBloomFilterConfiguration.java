c768e7eac9f21db29054a85a29f4e1bb14801fa8
Cymbospondylus <2810560593@qq.com>
2023-11-12 11:29:55 +0800

    docs: 修改后台管理系统用户注册布隆过滤器的注释

--- admin/src/main/java/site/bzyl/shortlink/admin/config/RBloomFilterConfiguration.java
+++ admin/src/main/java/site/bzyl/shortlink/admin/config/RBloomFilterConfiguration.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 /**
- * 布隆过滤器配置
+ *用户注册布隆过滤器配置
  */
 @Configuration
 public class RBloomFilterConfiguration {
