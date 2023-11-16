37cded7a6c269c1e8ed2d630fdd364cb93f73b7a
Cymbospondylus <2810560593@qq.com>
2023-11-16 23:49:40 +0800

    feature: 将短链接移入回收站后从缓存中删除

--- project/src/main/java/site/bzyl/shortlink/project/service/impl/RecycleBinServiceImpl.java
+++ project/src/main/java/site/bzyl/shortlink/project/service/impl/RecycleBinServiceImpl.java
@@ -3,20 +3,25 @@ package site.bzyl.shortlink.project.service.impl;
 import com.baomidou.mybatisplus.core.conditions.update.LambdaUpdateWrapper;
 import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
+import site.bzyl.shortlink.project.common.convention.exception.ClientException;
 import site.bzyl.shortlink.project.dao.entity.ShortLinkDO;
 import site.bzyl.shortlink.project.dao.mapper.ShortLinkMapper;
 import site.bzyl.shortlink.project.dto.req.RecycleBinSaveReqDTO;
 import site.bzyl.shortlink.project.service.RecycleBinService;
 
+import static site.bzyl.shortlink.project.common.constants.RedisKeyConstant.SHORT_LINK_REDIRECT_KEY;
 import static site.bzyl.shortlink.project.common.constants.ShortLinkConstant.ENABLE_STATUS_RECYCLE_BIN;
 
 /**
  * 短链接回收站业务实现类
  */
 @Service
+@RequiredArgsConstructor
 public class RecycleBinServiceImpl extends ServiceImpl<ShortLinkMapper, ShortLinkDO> implements RecycleBinService {
-
+    private final StringRedisTemplate stringRedisTemplate;
     @Override
     public void addShortLinkToRecycleBin(RecycleBinSaveReqDTO requestParam) {
         LambdaUpdateWrapper<ShortLinkDO> updateWrapper = Wrappers.lambdaUpdate(ShortLinkDO.class)
@@ -28,6 +33,10 @@ public class RecycleBinServiceImpl extends ServiceImpl<ShortLinkMapper, ShortLin
                 .enableStatus(ENABLE_STATUS_RECYCLE_BIN)
                 .build();
 
-        baseMapper.update(shortLinkDO, updateWrapper);
+        int update = baseMapper.update(shortLinkDO, updateWrapper);
+        if (update < 1) {
+            ClientException.cast("短链接移入回收站失败");
+        }
+        stringRedisTemplate.delete(String.format(SHORT_LINK_REDIRECT_KEY, requestParam.getFullShortUri()));
     }
 }
